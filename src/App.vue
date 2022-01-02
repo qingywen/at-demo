bdcd846395b8703f0f04838be563e40e741e1bd6
qingywen <qingywen@gmail.com>
2022-01-02 12:48:57 +0800

    调整代码

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@
     <div class="header"><img src="./assets/logo.png" /></div>
     <div class="imitate-box">
       <div
-        id="imitateInput"
-        v-html="content"
+        id="inputEdit"
         contenteditable
         @input="inputHandler"
         @keydown="keydownHandler"
@@ -12,13 +11,12 @@
       ></div>
       <div
         v-if="filteredList.length > 0"
-        id="imitateTip"
-        class="imitate-tip"
+        id="inputTip"
+        class="input-tip"
         :style="{ left: tipPos.x + 'px', bottom: tipPos.y +  'px' }"
       >
-        <div class="tip-arrow"></div>
         <div class="tip-box">
-          <el-radio-group ref="imitateTip" class="is-focus" v-model="picked">
+          <el-radio-group v-model="picked">
             <el-radio-button
               v-for="item in filteredList"
               :key="item.name"
@@ -34,6 +32,7 @@
             </el-radio-button>
           </el-radio-group>
         </div>
+        <div class="tip-arrow"></div>
       </div>
     </div>
   </div>
@@ -75,20 +74,16 @@ export default {
         x: 0,
         y: 0,
       },
-      insertFlag: null,
       cursorPos: "",
     };
   },
-  mounted() {},
   methods: {
-    inputHandler(e) {
+    inputHandler() {
 
       var selection = getSelection();
       lastEditRange = selection.getRangeAt(0);
 
-
       let content = lastEditRange.startContainer.data;
-
       // CONTENT不存在
       if (!content) {
         this.filteredList = [];
@@ -97,19 +92,16 @@ export default {
 
       let cursorPos = lastEditRange.startOffset;
       let lastAtPos = content.lastIndexOf("@", cursorPos - 1);
-
       // 没有@
       if (lastAtPos < 0) {
         this.filteredList = [];
         return;
       }
-
       // 为邮箱格式
       if(lastAtPos > 1 && /[A-Za-z0-9]/.test(content[lastAtPos - 1])) {
         this.filteredList = [];
         return;
       }
-
       // 节点为div
       if(selection.anchorNode.nodeName === 'DIV') {
         this.filteredList = [];
@@ -117,7 +109,6 @@ export default {
       }
 
       let filter = content.slice(lastAtPos + 1, cursorPos);
-
       this.tipPos = this.getTipPos(cursorPos);
       this.filteredList = [...this.list.filter(
         (item) => item.name.indexOf(filter) > -1
@@ -125,10 +116,12 @@ export default {
       this.picked = this.filteredList[0] && this.filteredList[0].name;
      
     },
+
     clickHandler(e) {
       var selection = getSelection();
       lastEditRange = selection.getRangeAt(0);
     },
+
     keydownHandler(e) {
       // 回车选中值插入
       if (e.keyCode === 13 && this.filteredList.length > 0) {
@@ -136,23 +129,7 @@ export default {
         e.preventDefault();
         e.stopPropagation();
       }
-      // 回车换行
-      if(e.keyCode === 13 && this.filteredList.length < 1) {
-        // console.log('回车换行')
-        // let imitateInput = document.getElementById('imitateInput')
-        // let range = document.createRange();
-        // let selection = getSelection();
-        // let offset = selection.focusOffset
-        // let content = imitateInput.innerHTML
-        // imitateInput.innerHTML = content.slice(0,offset) + '&nbsp;' +  content.slice(offset)
-        // range.setStart(imitateInput.childNodes[0], offset + 1);
-        // range.collapse(true);
-        // selection.removeAllRanges();
-        // selection.addRange(range);
-        // e.preventDefault();
-        // e.stopPropagation();
-        // return false
-      }
+
       // 上下键切换选中值 ArrowUp 38 ArrowDown 40
       if (e.keyCode === 38 && this.filteredList.length > 0) {
         let curIndex = this.filteredList.map(item=>item.name).indexOf(this.picked);
@@ -165,71 +142,66 @@ export default {
         this.picked = this.filteredList[nextIndex].name
       }
     },
+
     insertName(e, val) {
       if (e.target.tagName === "INPUT") return;
       this.filteredList = [];
-      // 在光标处插入选中名称
-      let imitateInput = document.getElementById("imitateInput");
-      imitateInput.focus();
+      let inputEdit = document.getElementById("inputEdit");
+
+      // 还原上次光标对象
+      inputEdit.focus();
       var selection = getSelection();
-      // 如果保存的有上次的光标对象
       if (lastEditRange) {
-        // 清除所有选区
         selection.removeAllRanges();
-        // 添加最后光标还原之前的状态
         selection.addRange(lastEditRange);
       }
+
+      // 获取插入字段
       let cursorPos = lastEditRange.startOffset
-      let cut = selection.anchorOffset - imitateInput.innerHTML.lastIndexOf('@', cursorPos - 1 ) - 1;
+      let cut = selection.anchorOffset - inputEdit.innerHTML.lastIndexOf('@', cursorPos - 1 ) - 1;
       val = val.slice(cut)
+
+      // 根据节点类型插入字段
       if (selection.anchorNode.nodeName != "#text") {
+        // 非文本节点
+        // 创建并插入节点
         var insertText = document.createTextNode(val);
-        if (imitateInput.childNodes.length > 0) {
-          for (var i = 0; i < imitateInput.childNodes.length; i++) {
+        if (inputEdit.childNodes.length > 0) {
+          for (var i = 0; i < inputEdit.childNodes.length; i++) {
             if (i == selection.anchorOffset) {
-              imitateInput.insertBefore(insertText, imitateInput.childNodes[i]);
+              inputEdit.insertBefore(insertText, inputEdit.childNodes[i]);
             }
           }
         } else {
-          imitateInput.appendChild(insertText);
+          inputEdit.appendChild(insertText);
         }
         // 创建新的光标对象
         var range = document.createRange();
-        // 将光标对象的范围界定为新建的表情节点
         range.selectNodeContents(insertText);
-        // 定位光标位置在表情节点的最大长度位置
         range.setStart(insertText, insertText.length);
-
-        // 将选区折叠为一个光标
         range.collapse(true);
-        // 清除所有光标对象
         selection.removeAllRanges();
-        // 添加新的光标对象
         selection.addRange(range);
-        // 如果是文本节点
       } else {
-        // 获取光标对象
+        // 如果是文本节点
+        // 在光标处插入文本
         var range = selection.getRangeAt(0);
-        // 获取光标对象的范围界定对象，一般就是textNode对象
         var textNode = range.startContainer;
-        // 获取光标位置
         var rangeStartOffset = range.startOffset;
-        // 在光标位置处插入
         textNode.insertData(rangeStartOffset, val);
-        // 添加了新内容，将光标移动到新的位置
-        range.setStart(textNode, rangeStartOffset + val.length);
 
-        // 将选区折叠为一个光标
+        // 修正光标位置
+        range.setStart(textNode, rangeStartOffset + val.length);
         range.collapse(true);
-        // 清除所有光标对象
         selection.removeAllRanges();
-        // 添加新的光标对象
         selection.addRange(range);
       }
+
       // 记录最后光标对象
       lastEditRange = selection.getRangeAt(0);
 
     },
+    
     getTipPos() {
       // 在光标处插入暂时性元素
       let selection = window.getSelection();
@@ -264,7 +236,7 @@ export default {
 .header {
   text-align: center;
 }
-#imitateInput {
+#inputEdit {
   width: 300px;
   line-height: 20px;
   max-height: 100px;
@@ -278,14 +250,14 @@ export default {
   overflow-y: auto;
   cursor: text;
 }
-#imitateInput:hover {
+#inputEdit:hover {
   border-color: #c0c4cc;
 }
-#imitateInput:focus {
+#inputEdit:focus {
   outline: none;
   border-color: #409eff;
 }
-.imitate-tip {
+.input-tip {
   width: 150px;
   /* overflow: hidden; */
   border-radius: 4px;
@@ -302,7 +274,7 @@ export default {
   background: #fff;
 }
 
-.imitate-tip:before {
+.input-tip:before {
   content: '';
   display: block;
   width: 22px;
@@ -314,7 +286,7 @@ export default {
   right: 0;
   margin: auto;
 }
-.imitate-tip:after {
+.input-tip:after {
   content: '';
   display: block;
   width: 12px;
